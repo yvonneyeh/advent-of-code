14440400a068f43f3c94fa31b91db663fdb17193
Yvonne Yeh <code@yvonneyeh.com>
2021-12-01 22:27:57 -0800

    Day 2

---
+++ 2021/02/tests.py
@@ -0,0 +1,12 @@
+from 02 import *
+
+def test_part_one():
+    assert part_one("test.txt") == 54
+
+def test_part_two():
+    assert part_two("test.txt") == 756
+
+if __name__ == "__main__":
+    test_part_one()
+    test_part_two()
+    print("Tests passed!")
