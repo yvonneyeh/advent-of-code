e7bd9540f7844024c507a3d9c168ae26bbb1b0b6
Yvonne Yeh <code@yvonneyeh.com>
2021-12-03 14:16:20 -0800

    WIP: Day 3

--- 2021/03/03.py
+++ 2021/03/03.py
@@ -1,14 +1,20 @@
 def open_file(input_file):
     with open(input_file) as file:
-        lines = file.readlines()
-    return lines
+        return [[int(column) for column in row] for row in file]
 
 def part_one(input_file):
 
-    gamma_rate = 0
-    epsilon_rate = 0
+    matrix = open_file(input_file)
+    transposed_matrix = zip(*matrix)
+    summed_rows = map(sum, transposed_matrix)
+    majority = list(map(lambda s: s > len(matrix)//2, summed_rows))
 
-    return power_consumption = gamma_rate * epsilon_rate
+    gamma   = ''.join('1' if b else '0' for b in majority)
+    epsilon = ''.join('0' if b else '1' for b in majority)
+
+    power_consumption = gamma * epsilon
+
+    return power_consumption
 
 
 def part_two(input_file):
