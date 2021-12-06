6d51560726a3e26f4b7078a6bbcf4e4d4eebc872
Yvonne Yeh <code@yvonneyeh.com>
2021-12-05 21:16:11 -0800

    Day 5

--- 2021/05/05.py
+++ 2021/05/05.py
@@ -53,11 +53,48 @@ def part_one(input_file):
 
 def part_two(input_file):
 
-    pass
+    lines = open_file(input_file)
+    vent_positions = {}
+    for line in lines:
+        [x1, y1], [x2, y2] = line
+        small_x = min(x1, x2)
+        big_x = max(x1, x2)
+        small_y = min(y1, y2)
+        big_y = max(y1, y2)
+        x_delta = big_x - small_x
+        y_delta = big_y - small_y
+        if x1 == x2:
+            for y in range(small_y, big_y + 1):
+                positions = (x1, y)
+                if positions in vent_positions:
+                    vent_positions[positions] += 1
+                else:
+                    vent_positions[positions] = 1
+        elif y1 == y2:
+            for x in range(small_x, big_x + 1):
+                positions = (x, y1)
+                if positions in vent_positions:
+                    vent_positions[positions] += 1
+                else:
+                    vent_positions[positions] = 1
+        elif x_delta == y_delta:
+            x_dir_coeff = 1 if x2 > x1 else -1
+            y_dir_coeff = 1 if y2 > y1 else -1
+            for i in range(x_delta + 1):
+                positions = (x1 + i * x_dir_coeff, y1 + i * y_dir_coeff)
+                if positions in vent_positions:
+                    vent_positions[positions] += 1
+                else:
+                    vent_positions[positions] = 1
+
+    overlaps = count_overlaps(vent_positions)
+    print("Part 2")
+    print("There are", overlaps, "points where at least two lines overlap")
+    return overlaps
 
 
 part_one_answer = part_one("input.txt")
 part_two_answer = part_two("input.txt")
 
-print(f"Answer #1 = {part_one_answer}")    #
-print(f"Answer #2 = {part_two_answer}")    #
+print(f"Answer #1 = {part_one_answer}")    #6572
+print(f"Answer #2 = {part_two_answer}")    #21466
